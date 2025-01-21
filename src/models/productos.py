a3dcc6e13fe2c245bebf0d24faedc6de2cbf5b15
CarlosMoreno1290 <camoreno@alimentoscarnicos.com.co>
2025-01-21 16:53:56 -0500

    ultimo avance 21/01/2025

---
+++ src/models/productos.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from src.models import session, Base
+from src.models.categorias import Categorias 
+
+
+class Productos(Base):
+    __tablename__ = 'productos'
+    id = Column(Integer, primary_key=True)
+    descripcion = Column(String(300), unique=True, nullable=False)
+    valor_unitario = Column(Float(10,8), nullable=False)
+    unidad_medida = Column(String(3), nullable=False)
+    cantidad_stock = Column(Float(10,8))
+    categoria = Column(Integer, ForeignKey('categorias.id'), nullable=False)
+
+    def __init__(self, descripcion, valor_unitario, unidad_medida, cantidad_stock, categoria):
+        self.descripcion = descripcion
+        self.valor_unitario = valor_unitario
+        self.unidad_medida = unidad_medida
+        self.cantidad_stock = cantidad_stock
+        self.categoria = categoria
+
+    def obtener_productos():
+        productos = session.query(Productos, Categorias).join(Categorias, Productos.categoria == Categorias.id).all()
+        print(productos)
+        return productos
+    
+    def agregar_producto(producto):
+        producto = session.add(producto)
+        session.commit()
+        return producto
+    
+    def eliminar_producto(id):
+        producto = session.query(Productos).get(id)        
+        session.delete(producto)
+        session.commit()
+        return producto
+
+    
